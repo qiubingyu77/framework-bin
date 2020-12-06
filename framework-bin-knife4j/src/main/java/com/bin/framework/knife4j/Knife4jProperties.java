254360c8efd2e9afcc1c2c3139042da41ce6fd8e
qiubingyu77 <qiubingyu77@163.com>
2020-12-06 16:26:24 +0800

    新增版本好

--- framework-bin-knife4j/src/main/java/com/bin/framework/knife4j/Knife4jProperties.java
+++ framework-bin-knife4j/src/main/java/com/bin/framework/knife4j/Knife4jProperties.java
@@ -1,5 +1,8 @@
 package com.bin.framework.knife4j;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties
 public class Knife4jProperties {
     public static void main(String[] args) {
     }
